6e2864803c2c481ad2520b9188dd78b6f0c6918c
I.Suvorova <i.suvorova@x5.ru>
2021-07-23 02:33:44 +0300

    remove debug_toolbar

--- petme/settings/development.py
+++ petme/settings/development.py
@@ -5,6 +5,6 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
-INSTALLED_APPS.append('debug_toolbar')
-MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+# INSTALLED_APPS.append('debug_toolbar')
+# MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 INTERNAL_IPS = ['127.0.0.1']
