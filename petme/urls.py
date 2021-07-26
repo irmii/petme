9e94fb194d883d0e4a82bae211c944f69dd075fe
I.Suvorova <i.suvorova@x5.ru>
2021-07-26 15:00:34 +0300

    добавлена авторизация через google

--- petme/urls.py
+++ petme/urls.py
@@ -1,7 +1,15 @@
+
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
+from django.views.generic import TemplateView
 from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('authentication.urls', namespace='authentication')),
+    path('accounts/', include('allauth.urls')),
+    path('', TemplateView.as_view(template_name="index.html")),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view()),
 ]
