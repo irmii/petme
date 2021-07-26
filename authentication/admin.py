8f28547092fcd8b40e200dbffdbc6aafa3b2d0b6
I.Suvorova <i.suvorova@x5.ru>
2021-07-26 15:33:58 +0300

    админка User

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,2 +1,13 @@
 from django.contrib import admin
 
+from authentication.models import (
+    User,
+)
+from common.utils import all_fields_names
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Админка User."""
+
+    list_display = all_fields_names(User)
