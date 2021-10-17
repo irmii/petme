92342b6edf48a3302056e4adbd18560c18046f86
i.suvorova <i.suvorova@x5.ru>
2021-10-17 22:38:39 +0300

    повторная инициализация dev ветки

--- petme/settings/base.py
+++ petme/settings/base.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # manage.py createdefaultsuperuser (command)
 TEST_DEFAULT_ADMIN_USERNAME = config('TEST_DEFAULT_ADMIN_USERNAME', default='admin')
 TEST_DEFAULT_ADMIN_PASSWORD = config('TEST_DEFAULT_ADMIN_PASSWORD', default='superuser')
-TEST_DEFAULT_ADMIN_EMAIL = config('TEST_DEFAULT_ADMIN_PASSWORD', default='zazzzu@yandex.ru')
+TEST_DEFAULT_ADMIN_EMAIL = config('TEST_DEFAULT_ADMIN_PASSWORD', default='bup@bip.ru')
 
 
 # Использовать кастомную модель
