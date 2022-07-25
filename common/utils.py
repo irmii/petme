ab17b192b19a540b77e0c0bc99dddb9e5fc70683
irmi <im@irmio.ru>
2022-07-25 22:13:28 +0300

    не своровала, а одолжила

--- common/utils.py
+++
@@ -1,9 +0,0 @@
-"""Модуль утилит."""
-
-
-def all_fields(model):
-    return [field for field in model._meta.fields]
-
-
-def all_fields_names(model):
-    return [field.name for field in all_fields(model)]
